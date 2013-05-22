7263f49ff5f7d6f235a3156ae9fe9020902499b6
Wang Rao <wrao@utexas.edu>
2013-05-22 14:12:40 -0500

    added the help page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -41,4 +41,18 @@ describe PagesController do
 	  	:content => "Ruby on Rails Tutorial Sample App | About")
 	end
   end
+
+  describe "GET 'Help'" do
+  	it "should be successful" do
+	  get 'help'
+	  response.should be_success
+	end
+
+	it "should have title: About" do
+	  get 'help'
+	  response.should have_selector("title",
+	  	:content => "Ruby on Rails Tutorial Sample App | Help")
+	end
+  end
+
 end
